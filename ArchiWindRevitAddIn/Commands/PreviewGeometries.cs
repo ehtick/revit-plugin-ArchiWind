bc6102ad955d7e1a999a8f411a8a2a6be8319839
Federico Ravasio <ravasio.federico@gmail.com>
2025-08-25 15:50:53 +0200

    refactor: extract view-related utilities

--- ArchiWindRevitAddIn/Commands/PreviewGeometries.cs
+++ ArchiWindRevitAddIn/Commands/PreviewGeometries.cs
@@ -1,7 +1,7 @@
-﻿using Autodesk.Revit.Attributes;
+﻿using ArchiWindRevitAddIn.Views;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.UI;
 using Nice3point.Revit.Toolkit.External;
-using System.Collections.Immutable;
 
 namespace ArchiWindRevitAddIn.Commands
 {
@@ -12,11 +12,6 @@ namespace ArchiWindRevitAddIn.Commands
     public class PreviewGeometries : ExternalCommand
     {
         private const string TRANSACTION_NAME = "ArchiWind 3D views setup";
-
-        private const string BUILDING_VIEW = "ArchiWind - Building View";
-        private const string SURROUNDINGS_VIEW = "ArchiWind - Surroundings View";
-        private const string TERRAIN_VIEW = "ArchiWind - Terrain View";
-        private const string VEGETATION_VIEW = "ArchiWind - Vegetation View";
 
         public override void Execute()
         {
@@ -40,17 +35,22 @@ namespace ArchiWindRevitAddIn.Commands
             {
                 t.Start();
 
-                var buildingView = CreateView(BUILDING_VIEW, doc, ActiveView);
-                var surroundingsView = CreateView(SURROUNDINGS_VIEW, doc, ActiveView);
-                var vegetationView = CreateView(VEGETATION_VIEW, doc, ActiveView);
-                var terrainView = CreateView(TERRAIN_VIEW, doc, ActiveView);
+                var buildingView = Utils.CreateView(doc, ActiveView, Utils.BUILDING_VIEW);
+                var surroundingsView = Utils.CreateView(doc, ActiveView, Utils.SURROUNDINGS_VIEW);
+                var vegetationView = Utils.CreateView(doc, ActiveView, Utils.VEGETATION_VIEW);
+                var terrainView = Utils.CreateView(doc, ActiveView, Utils.TERRAIN_VIEW);
 
-                OnlyShowCategories(doc, buildingView, Models.Categories.DefaultBuildingCategories);
-                OnlyShowCategories(doc, surroundingsView, Models.Categories.DefaultSurroundingsCategories);
-                OnlyShowCategories(doc, vegetationView, Models.Categories.DefaultVegetationCategories);
-                OnlyShowCategories(doc, terrainView, Models.Categories.DefaultTerrainCategories);
+                Utils.OnlyShowCategories(doc, buildingView, Models.Categories.DefaultBuildingCategories);
+                Utils.OnlyShowCategories(doc, surroundingsView, Models.Categories.DefaultSurroundingsCategories);
+                Utils.OnlyShowCategories(doc, vegetationView, Models.Categories.DefaultVegetationCategories);
+                Utils.OnlyShowCategories(doc, terrainView, Models.Categories.DefaultTerrainCategories);
 
                 t.Commit();
+
+                UiDocument.RequestViewChange(buildingView);
+                UiDocument.RequestViewChange(surroundingsView);
+                UiDocument.RequestViewChange(vegetationView);
+                UiDocument.RequestViewChange(terrainView);
             }
             catch (Exception ex)
             {
@@ -83,70 +83,6 @@ namespace ArchiWindRevitAddIn.Commands
             //customExporter.Export(this.ActiveView);
 
             //currentDocument.Export("C:\\Users\\your mom\\Desktop", "terrain.stl", exportOptions);
-        }
-
-        private static void OnlyShowCategories(Document doc, View3D view, ImmutableHashSet<BuiltInCategory> showCategories)
-        {
-            foreach (Category cat in doc.Settings.Categories)
-            {
-                if (!view.CanCategoryBeHidden(cat.Id))
-                {
-                    continue;
-                }
-
-#if REVIT2023_OR_GREATER
-                var asBuiltInCategory = cat.BuiltInCategory;
-#else
-                var asBuiltInCategory = (BuiltInCategory)cat.Id.IntegerValue;
-#endif
-
-                view.SetCategoryHidden(cat.Id, !showCategories.Contains(asBuiltInCategory));
-            }
-        }
-
-        private static void DeleteViewIfExists(string name, Document doc)
-        {
-            try
-            {
-                View view = new FilteredElementCollector(doc)
-                    .OfCategory(BuiltInCategory.OST_Views)
-                    .WhereElementIsNotElementType()
-                    .Cast<View>()
-                    .First(x => x.Name == name);
-
-                doc.Delete(view.Id);
-            }
-            catch (InvalidOperationException)
-            {
-                return;
-            }
-        }
-
-        private static View3D CreateView(string name, Document doc, View threeDView)
-        {
-            DeleteViewIfExists(name, doc);
-
-            var view = DuplicateThreeDView(doc, threeDView);
-
-            view.Name = name;
-            view.DetailLevel = ViewDetailLevel.Coarse;
-            view.DisplayStyle = DisplayStyle.Shading;
-
-            return view;
-        }
-
-        private static View3D DuplicateThreeDView(Document doc, View threeDView)
-        {
-            var viewId = threeDView.Duplicate(ViewDuplicateOption.Duplicate);
-
-            if (doc.GetElement(viewId) is not View3D view)
-            {
-                throw new Exception("failed to duplicate 3D View");
-            }
-
-            view.ViewTemplateId = ElementId.InvalidElementId;
-
-            return view;
         }
     }
 }
---
+++ ArchiWindRevitAddIn/Views/Utils.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Immutable;
+
+namespace ArchiWindRevitAddIn.Views
+{
+    internal class Utils
+    {
+        public const string BUILDING_VIEW = "ArchiWind - Building";
+        public const string SURROUNDINGS_VIEW = "ArchiWind - Surroundings";
+        public const string TERRAIN_VIEW = "ArchiWind - Terrain";
+        public const string VEGETATION_VIEW = "ArchiWind - Vegetation";
+
+        public static int CountElementsInView(Document doc, View3D view, ImmutableHashSet<BuiltInCategory> categories)
+        {
+            return categories.Select(cat =>
+            {
+                return new FilteredElementCollector(doc, view.Id)
+                .OfCategory(cat)
+                .WhereElementIsNotElementType()
+                .GetElementCount();
+            }).Sum();
+        }
+
+        public static void OnlyShowCategories(Document doc, View3D view, ImmutableHashSet<BuiltInCategory> showCategories)
+        {
+            foreach (Category cat in doc.Settings.Categories)
+            {
+                if (!view.CanCategoryBeHidden(cat.Id))
+                {
+                    continue;
+                }
+
+#if REVIT2023_OR_GREATER
+                var asBuiltInCategory = cat.BuiltInCategory;
+#else
+                var asBuiltInCategory = (BuiltInCategory)cat.Id.IntegerValue;
+#endif
+
+                view.SetCategoryHidden(cat.Id, !showCategories.Contains(asBuiltInCategory));
+            }
+        }
+
+        public static View? FindView(Document doc, string name)
+        {
+            try
+            {
+                return new FilteredElementCollector(doc)
+                    .OfCategory(BuiltInCategory.OST_Views)
+                    .WhereElementIsNotElementType()
+                    .Cast<View>()
+                    .First(x => x.Name == name);
+            }
+            catch (InvalidOperationException)
+            {
+                return null;
+            }
+        }
+
+        public static void DeleteViewIfExists(Document doc, string name)
+        {
+            View? view = FindView(doc, name);
+
+            if (view != null)
+            {
+                doc.Delete(view.Id);
+            }
+        }
+
+        public static View3D CreateView(Document doc, View threeDView, string name)
+        {
+            DeleteViewIfExists(doc, name);
+
+            var view = DuplicateThreeDView(doc, threeDView);
+
+            view.Name = name;
+            view.DetailLevel = ViewDetailLevel.Coarse;
+            view.DisplayStyle = DisplayStyle.Shading;
+
+            return view;
+        }
+
+        private static View3D DuplicateThreeDView(Document doc, View threeDView)
+        {
+            var viewId = threeDView.Duplicate(ViewDuplicateOption.Duplicate);
+
+            if (doc.GetElement(viewId) is not View3D view)
+            {
+                throw new Exception("failed to duplicate 3D View");
+            }
+
+            view.ViewTemplateId = ElementId.InvalidElementId;
+
+            return view;
+        }
+    }
+}
